5bce7c89075fe3650ae37039baada0f33d986d6c
dhinesh kumar <dhineshk0430@gmail.com>
2026-09-23 11:43:52 +0530

    updated

---
+++ Js/Day2/Task2/assets/js/index.js
@@ -0,0 +1,12 @@
+var userName 
+console.log(userName)
+var age
+console.log(age)
+var fActor
+console.log(fActor)
+var price
+console.log(price)
+var menuSection
+console.log(menuSection)
+var viewOrder
+console.log(viewOrder)
---
+++ Js/Day2/Task3/assets/js/index.js
@@ -0,0 +1,111 @@
+//var - strings
+var myName="Dhinesh kumar"
+console.log(myName)
+
+var favouriteSport ="Cricket"
+console.log(favouriteSport)
+
+var favouriteActor = "ajith"
+console.log(favouriteActor)
+
+var favouriteMovie="nkp"
+console.log(favouriteMovie)
+
+var yourPassion ="Athlete"
+console.log(yourPassion)
+
+var yourProfession = "Developer"
+console.log(yourProfession)
+
+var favouriteFood="Chicken Rice"
+console.log(favouriteFood)
+
+var yourNative ="Tiruvannamalai"
+console.log(yourNative)
+
+var yourGoal = "Good Carrer in IT"
+console.log(yourGoal)
+
+var activity="Watching Movies"
+console.log(activity)
+
+//var-numbers
+
+var age = 20
+console.log(age)
+
+var height = 174
+console.log(height)
+
+var weight = 54
+console.log(weight)
+
+var marks = 85
+console.log(marks)
+
+var speed = 100
+console.log(speed)
+
+var salary = 30000
+console.log(salary)
+
+var score = 95
+console.log(score)
+
+var distance = 10
+console.log(distance)
+
+var quantity = 5
+console.log(quantity)
+
+var price = 500
+console.log(price)
+
+//var-boolean
+
+var isStudent = true
+console.log(isStudent)
+
+var isAthlete = true
+console.log(isAthlete)
+
+var isDeveloper = true
+console.log(isDeveloper)
+
+var isWorking = false
+console.log(isWorking)
+
+var isMarried = false
+console.log(isMarried)
+
+var isAvailable = true
+console.log(isAvailable)
+
+var hasBike = false
+console.log(hasBike)
+
+var isLearning = true
+console.log(isLearning)
+
+//var-undefined
+var firstName
+console.log(firstName)
+
+var lastName
+console.log(lastName)
+
+var address
+console.log(address)
+
+var phoneNumber
+console.log(phoneNumber)
+
+var email
+console.log(email)
+
+//var-null
+var middleName = null
+console.log(middleName)
+
+var selectedColor = null
+console.log(selectedColor)
