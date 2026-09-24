83d3d840350fb75958678c6e1b876d4f38cee118
dhinesh kumar <dhineshk0430@gmail.com>
2026-09-24 12:13:42 +0530

    updated

---
+++ Js/Day3/class-task/assets/index.js
@@ -0,0 +1,14 @@
+let firstNumber=10
+let secondNumber =2000
+let thirdNumber=1990
+
+secondNumber=firstNumber
+let total=1990
+let answer= firstNumber+secondNumber
+console.log(total+answer)
+
+//1.string-18 times create
+// reasign - +=
+
+//2. number - 13  times -create pannanum = reaasign with +=
+
---
+++ Js/Day3/task-1/assets/index.js
@@ -0,0 +1,46 @@
+
+//1.string-18 times create
+// reasign - +=
+
+//2. number - 13  times -create pannanum = reaasign with +=
+
+
+var firstName = "Dinesh"
+console.log(firstName);
+
+firstName = "Dhinesh"
+console.log(firstName)
+
+
+var lastName = "kumar"
+console.log(lastName);
+
+lastName =" Kumar"
+console.log(lastName);
+
+
+var fullName = firstName + lastName
+console.log(fullName);
+
+fullName += " R"
+console.log(fullName);
+
+
+var weekStart = "monday"
+console.log(weekStart);
+
+var weekEnd = "sunday"
+console.log(weekEnd);
+
+console.log(weekStart+weekEnd);
+
+
+weekStart="Sunday"
+console.log(weekStart);
+weekEnd="Saturday"
+console.log(weekEnd);
+
+console.log(weekStart+weekEnd);
+
+
+
